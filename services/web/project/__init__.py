6b85a8349dfcdb7f81337ae410848a44263efcaa
Milton <mi_correo37@hotmail.com>
2020-10-30 22:40:22 -0600

    Logica de impresión de resultados en pantalla con Bootstrap

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,14 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, flash,render_template
 from .reinas import coloca_reinas
 from .models import soluciones, init_db
 from sqlalchemy import update
 from .db import Session
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 init_db()
 
+# in a real app, these should be configured through Flask-Appconfig
+app.config['SECRET_KEY'] = 'devkey'
+app.config['RECAPTCHA_PUBLIC_KEY'] = \
+    '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
 @app.route("/")
 def hello_world():
+  flash('error message', 'error')
   # query a la tabla soluciones 
   # donde reinas = 4
   # selet from soluciones where reinas = 4
@@ -16,7 +24,12 @@ def hello_world():
   # else calculamos e insertamos
   solucion = coloca_reinas(4)
   print(solucion)
-
+  solus = Session.query(soluciones).filter_by(reinas=4).first()
+  context = {
+      'solucion': str(solucion),
+      'solus' : solucion
+  }
+  print(solus.soluciones)
   #solus = Session.query(soluciones).filter_by(reinas=4).first()
   #print(solus.soluciones)
 
@@ -26,5 +39,16 @@ def hello_world():
   #ins = soluciones(4,solucion)
   #Session.add(ins)
   #Session.commit()
-  Session.commit() #para hacer commit al final
-  return jsonify(solucion)
+  #Session.commit() #para hacer commit al final
+  return render_template('base.html', **context)
+
+
+@app.route('/resuelve/<int:n>', methods=['GET'])
+def resuelve(n):
+    solucion = coloca_reinas(n)
+    context = {
+      'solus' : solucion
+    }
+    return render_template('base.html', **context)
+
+
--- services/web/project/db.py
+++ services/web/project/db.py
@@ -1,13 +1,14 @@
+#https://github.com/pallets/flask-sqlalchemy
+#https://testdriven.io/blog/dockerizing-flask-with-postgres-gunicorn-and-nginx/
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#app.config.from_object("project.config.Config")
 
 Session = sessionmaker()
 DATABASE_URI ='postgresql://milton:cuenca@db:5432/soluciones_reinas'
 engine = create_engine(DATABASE_URI, convert_unicode=True)
-Base = declarative_base() # Migraciones con clases python
+Base = declarative_base() 
 Session.configure(bind=engine)
 Session = Session(autocommit=False)
 
